ce02b3d2b91d430f3d40908aa7a0d536f3cb5a71
Nivrutti pawar <159421324+Niruttipawar@users.noreply.github.com>
2024-08-10 21:50:12 +0530

    Create Calculator.c

---
+++ Calculator.c
@@ -0,0 +1,8 @@
+#include<stdio.h> //heder file
+int main() //main function
+ float a = 12; // value of a
+ float b = 15; // value of b
+ float c = a + b; // formula of sum
+ printf("%d",c); // command for sum
+ return 0;     // code is done
+} 
