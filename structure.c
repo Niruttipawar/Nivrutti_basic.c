8c2440df13a9fa6eb017c9830ade3bb7c2def390
Nivrutti pawar <159421324+Niruttipawar@users.noreply.github.com>
2025-01-25 21:22:09 +0530

    Update structure.C
    In a structure we store the data of one employee using struct in C lang

--- structure.c
+++ structure.c
@@ -1,9 +1,22 @@
-#include<stdio.h> // header file
-int main() {    // main function
-
-// type here code
-
-
-return 0; // ending of code
-}
+#include <stdio.h>
+// Define a structure
+struct Person {
+    char name[50];
+    int age;
+    float salary;
+};
+int main() {
+    // Declare a structure variable
+ struct Person person1;
+  // Assign values to the structure members
+    strcpy(person1.name, "John");
+ person1.age = 30;
+ person1.salary = 50000.50;
+  // Access and print structure members
  
+ printf("Name: %s\n", person1.name);
+    printf("Age: %d\n", person1.age);
+    printf("Salary: %.2f\n", person1.salary);
+
+return 0;
+}
